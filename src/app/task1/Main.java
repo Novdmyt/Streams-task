fd5ba55b1b3b057c6e53a2c80e5f2acf533c0c7e
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-03 22:11:18 +0300

    Add method printNames

--- src/app/task1/Main.java
+++ src/app/task1/Main.java
@@ -2,6 +2,7 @@ package app.task1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class Main {
@@ -11,5 +12,9 @@ public class Main {
     }
     private static Stream<String> filterNames(Stream<String> names, char targetLetter) {
         return names.filter(name -> name.startsWith(String.valueOf(targetLetter)));
+    }
+    private static void printNames(Stream<String> names) {
+        AtomicInteger counter = new AtomicInteger(1);
+        names.forEach(name -> System.out.println(counter.getAndIncrement() + ") " + name));
     }
 }
