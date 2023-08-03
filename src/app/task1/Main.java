326945e59e7c6387ce01ba112ead136f2eebdaf2
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-03 22:09:51 +0300

    Add method listOfNames

---
+++ src/app/task1/Main.java
@@ -0,0 +1,11 @@
+package app.task1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+   public static List<String> listOfNames() {
+        return Arrays.asList("Alice", "Bob", "Charlie", "Daniel", "Eva", "Frank", "Gina", "Hanna", "Alex","Arina");
+    }
+}
