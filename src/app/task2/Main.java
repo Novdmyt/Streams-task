6cebf161eaf538b41a19ce6e0bba7984d44be052
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-03 22:19:52 +0300

    Add method filterProducts

--- src/app/task2/Main.java
+++ src/app/task2/Main.java
@@ -2,9 +2,9 @@ package app.task2;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.stream.Stream;
 public class Main {
-    private static Map<String, Double> getProducts() {
+    public static Map<String, Double> getProducts() {
         Map<String, Double> products = new HashMap<>();
         products.put("Tomato", 2.50);
         products.put("Pepsi", 1.05);
@@ -15,4 +15,7 @@ public class Main {
         return products;
     }
 
+    public static Stream<Map.Entry<String, Double>> filterProducts(Stream<Map.Entry<String, Double>> products) {
+        return products.filter(entry -> entry.getValue() <= 2);
+    }
 }
