02ce9cb41c1ad121ea6470b68268e5a4acddc36f
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-08-03 22:10:37 +0300

    Add method filterNames

--- src/app/task1/Main.java
+++ src/app/task1/Main.java
@@ -2,10 +2,14 @@ package app.task1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
    public static List<String> listOfNames() {
         return Arrays.asList("Alice", "Bob", "Charlie", "Daniel", "Eva", "Frank", "Gina", "Hanna", "Alex","Arina");
+    }
+    private static Stream<String> filterNames(Stream<String> names, char targetLetter) {
+        return names.filter(name -> name.startsWith(String.valueOf(targetLetter)));
     }
 }
